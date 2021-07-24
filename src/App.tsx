cba5409e9de7621f88c38a653d40022904beb5b6
IlyaBelousov <77740267+IlyaBelousov@users.noreply.github.com>
2021-07-24 12:13:59 +0300

    Class component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {Route} from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import {Users} from './components/Users/Users';
 import {UsersContainer} from './components/Users/UsersContainer';
 
 
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import User from './User';
 import s from './User.module.css';
 import {AppStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
-import {FollowAC, SetUsersAC, UserDataType, UserStateType} from '../../redux/users-reducer';
+import {FollowAC, SetUsersAC, UserDataType} from '../../redux/users-reducer';
 import axios from 'axios';
+
 
 type UsersPropsType = UsersMapStateToPropsType & UsersMapDispatchToPropsType
 
@@ -16,47 +17,51 @@ type UsersMapDispatchToPropsType = {
     setUsers: (items: Array<UserDataType>) => void
 }
 
-export const Users = (props: UsersPropsType) => {
+export class Users extends React.Component<UsersPropsType> {
 
-    if (props.items.length === 0) {
-        axios.get<{ items: UserDataType[] }>('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
+    componentDidMount() {
+        if (this.props.items.length === 0) {
+            axios.get<{ items: UserDataType[] }>('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                });
+        }
     }
-    return (
-        <div>
 
-            {
-                props.items.map(u => {
-                    const FollowHandler = () => {
-                        props.follow(u.id);
-                    };
-                    return (
-                        <div key={u.id} className={s.wrapper}>
-                            <div className={s.container}>
-                                <div className={s.usersBlock}>
-                                    <User key={u.id} name={u.name} photoUrl={"https://insights.mgm-tp.com/wp-content/uploads/2019/04/default-avatar.png"}/>
-                                    <p className={s.status}>{u.status}</p>
-                                    <div>
-                                        <button
-                                            onClick={FollowHandler}
-                                            className={s.userButton}>
-                                            {u.followed ? 'UNFOLLOW' : 'FOLLOW'}
-                                        </button>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.items.map(u => {
+                        const FollowHandler = () => {
+                            this.props.follow(u.id);
+                        };
+                        return (
+                            <div key={u.id} className={s.wrapper}>
+                                <div className={s.container}>
+                                    <div className={s.usersBlock}>
+                                        <User key={u.id} name={u.name}
+                                              photoUrl={'https://insights.mgm-tp.com/wp-content/uploads/2019/04/default-avatar.png'}/>
+                                        <p className={s.status}>{u.status}</p>
+                                        <div>
+                                            <button
+                                                onClick={FollowHandler}
+                                                className={s.userButton}>
+                                                {u.followed ? 'UNFOLLOW' : 'FOLLOW'}
+                                            </button>
+                                        </div>
                                     </div>
+                                    {/*<div className={s.location}>
+                                        {/!*<div className={s.country}>{u.location.country}</div>
+                                            <div className={s.city}>{u.location.city}</div>*!/}
+                                            </div>*/}
                                 </div>
-                                <div className={s.location}>
-                                    {/*<div className={s.country}>{u.location.country}</div>
-                                    <div className={s.city}>{u.location.city}</div>*/}
-                                </div>
-                            </div>
-                        </div>);
-                })
-            }
-        </div>
-    );
-};
+                            </div>);
+                    })}
+            </div>);
+    }
+}
+
 
 export const UsersMapStateToProps = (state: AppStateType): UsersMapStateToPropsType => {
     return {
@@ -73,4 +78,3 @@ export const UsersMapDispatchToProps = (dispatch: Dispatch): UsersMapDispatchToP
         }
     };
 };
-
